5790dd5f5315138949029ffdcf77bd346bfab7a6
Konstantin Ligus <kostya1989com@gmail.com>
2023-04-07 17:53:09 +0300

    add config

--- src/pages/LoginAndRegisterPage/LoginAndRegisterPage.jsx
+++ src/pages/LoginAndRegisterPage/LoginAndRegisterPage.jsx
@@ -1,8 +1,16 @@
 import * as firebaseui from 'firebaseui';
 import firebase from 'firebase/compat/app';
 import { useEffect } from 'react';
-import { firebaseConfig } from '../../secret';
 
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBVClOKPEUth1HcHcd-qRVAv_l97ywtPrs',
+  authDomain: 'authenticateforapps.firebaseapp.com',
+  projectId: 'authenticateforapps',
+  storageBucket: 'authenticateforapps.appspot.com',
+  messagingSenderId: '57680612991',
+  appId: '1:57680612991:web:cfc648150c2a0fa65c794a',
+  measurementId: 'G-2LQFL7WCPQ',
+};
 firebase.initializeApp(firebaseConfig);
 
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
